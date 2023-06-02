d23e729b34174f309d687782c829a79514d5f5f9
serraguti <pacoserranox@gmail.com>
2023-06-02 13:03:31 +0200

    Add project files.

---
+++ AWSSecretsManagerClient/Models/KeysModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSSecretsManagerClient.Models
+{
+    public class KeysModel
+    {
+        public string MySql { get; set; }
+        public string S3Bucket { get; set; }
+        public string ApiPersonajes { get; set; }
+        public string Champions { get; set; }
+    }
+}
